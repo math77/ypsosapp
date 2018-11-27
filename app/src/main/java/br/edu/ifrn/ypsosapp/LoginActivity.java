4cb2931e697aa42eb882d40932781b10d702c51e
Matheus Emanuel <matheusemanuel745@gmail.com>
2018-11-27 19:36:25 -0300

    Primeiro commit

---
+++ app/src/main/java/br/edu/ifrn/ypsosapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package br.edu.ifrn.ypsosapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import retrofit2.Retrofit;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
