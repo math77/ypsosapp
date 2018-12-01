7c418e8c47f3aadca426663badb9c1d95be7ec2e
Matheus Emanuel <matheusemanuel745@gmail.com>
2018-11-30 23:17:27 -0300

    Alteração em CadastroActivity

--- app/src/main/java/br/edu/ifrn/ypsosapp/activitys/CadastroActivity.java
+++ app/src/main/java/br/edu/ifrn/ypsosapp/activitys/CadastroActivity.java
@@ -3,6 +3,8 @@ package br.edu.ifrn.ypsosapp.activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.edu.ifrn.ypsosapp.R;
+
 public class CadastroActivity extends AppCompatActivity {
 
     @Override
