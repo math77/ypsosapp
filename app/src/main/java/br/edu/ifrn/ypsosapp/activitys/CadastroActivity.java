89e83b57888e21c41fd81dac4ce64157fe458050
Matheus Emanuel <matheusemanuel745@gmail.com>
2018-12-04 09:56:56 -0300

    Add README

--- app/src/main/java/br/edu/ifrn/ypsosapp/activitys/CadastroActivity.java
+++ app/src/main/java/br/edu/ifrn/ypsosapp/activitys/CadastroActivity.java
@@ -1,5 +1,6 @@
 package br.edu.ifrn.ypsosapp.activitys;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,14 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    private void cadastrar(){
+        Dialog dialog = new Dialog(CadastroActivity.this);
+        dialog.setTitle("Cadastrando...");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        //criar código para cadastrar
     }
 }
